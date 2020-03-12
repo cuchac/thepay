cc02e7284bd78e7536a792c702064a32a245431b
Michal Čihař <michal@cihar.com>
2020-03-12 17:21:02 +0100

    Adjust test to new data

--- thepay/test_gateApi.py
+++ thepay/test_gateApi.py
@@ -5,12 +5,14 @@ import uuid
 
 from thepay.config import Config
 from thepay.gateApi import GateApi, GateError
+from thepay.dataApi import DataApi
 
 
 class GateApiTests(unittest.TestCase):
     def setUp(self):
         self.config = Config()
         self.gateApi = GateApi(self.config)
+        self.dataApi = DataApi(self.config)
 
     def test_invalid_cardCreateRecurrentPayment(self):
         with self.assertRaises(GateError):
@@ -21,8 +23,11 @@ class GateApiTests(unittest.TestCase):
             )
 
     def test_cardCreateRecurrentPayment(self):
+        for payment in self.dataApi.getPayments(method_ids=[21, 31], state_ids=[2]).payments.payment:
+            if payment.merchantData and payment.recurring and payment.deposit:
+                break
         self.gateApi.cardCreateRecurrentPayment(
-            'c53be2ae-0b84-46e2-90f9-03c144a1a328',
+            payment.merchantData,
             uuid.uuid4(),
-            10
+            1
         )
